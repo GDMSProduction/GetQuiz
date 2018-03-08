8d26e3c82c63d4afa4cce19156734e33841f2e60
Jesus Fragoso <JAFragoso@student.fullsail.edu>
2018-03-08 15:54:29 -0500

    Put numbers on the seekbar for the Background Music and the Sound Effects, and set the bars to full when you load up the settings

--- TriviaGame/app/src/main/java/fullsailclass/triviagame/SettingsScreen.java
+++ TriviaGame/app/src/main/java/fullsailclass/triviagame/SettingsScreen.java
@@ -5,9 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class SettingsScreen extends AppCompatActivity {
+    int progressBackMusic=100;
+    SeekBar backMusic;
+    TextView backmusicNum;
 
+    int progressSoundEffect=100;
+    SeekBar soundEffect;
+    TextView SoundEffectNum;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +23,72 @@ public class SettingsScreen extends AppCompatActivity {
 
         configureSettingBackButton();
         configureCreditsButton();
+        configureBackMusicSeekbar();
+        configureSoundEffectSeekbar();
+
+
+
+    }
+
+    private void configureBackMusicSeekbar() {
+        backMusic= (SeekBar) findViewById(R.id.BackMusicSeekbar);
+        backMusic.setMax(100);
+        backMusic.setProgress(progressBackMusic);
+
+        backmusicNum = (TextView) findViewById(R.id.BackMusicNumText);
+        backmusicNum.setText(""+progressBackMusic);
+
+        backMusic.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                progressBackMusic =i;
+                backmusicNum.setText(""+progressBackMusic);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+    }
+    private void configureSoundEffectSeekbar() {
+        soundEffect= (SeekBar) findViewById(R.id.SoundEffectsSeekbar);
+        soundEffect.setMax(100);
+        soundEffect.setProgress(progressSoundEffect);
+
+        SoundEffectNum = (TextView) findViewById(R.id.SoundEffectsNumText);
+        SoundEffectNum.setText(""+progressSoundEffect);
+
+        soundEffect.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                progressSoundEffect =i;
+                SoundEffectNum.setText(""+progressSoundEffect);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
     }
 
     public void configureSettingBackButton() {
