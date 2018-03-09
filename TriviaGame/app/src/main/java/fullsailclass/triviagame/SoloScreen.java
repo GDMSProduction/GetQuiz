f335fe58f50779269a278042c7dca332b6000be0
Jose Otero Perez <JJOteroPerez@fullsail.edu>
2018-03-08 19:00:28 -0500

    Populated a list of QandA class!

--- TriviaGame/app/src/main/java/fullsailclass/triviagame/SoloScreen.java
+++ TriviaGame/app/src/main/java/fullsailclass/triviagame/SoloScreen.java
@@ -6,8 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class SoloScreen extends AppCompatActivity {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class SoloScreen extends AppCompatActivity {
+    List<QandA> list = new ArrayList<QandA>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,6 +57,8 @@ public class SoloScreen extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(SoloScreen.this, QuestionsScreen.class));
+                ReadQuestion r = new ReadQuestion();
+                OpenFile("game_questions.txt");
             }
 
         });
@@ -67,6 +74,7 @@ public class SoloScreen extends AppCompatActivity {
 
         });
     }
+
     public void configureMusicButton() {
         Button music = (Button) findViewById(R.id.Music);
         music.setOnClickListener(new View.OnClickListener() {
@@ -100,4 +108,46 @@ public class SoloScreen extends AppCompatActivity {
         });
     }
 
+
+    public void OpenFile(String test) {
+
+        Scanner s = null;
+        String question, a1, a2, a3, a4;
+        try {
+            s = new Scanner(new BufferedReader(new InputStreamReader(getAssets().open(test), "UTF-8")));
+
+            while (s.hasNext()) {
+
+                s.useDelimiter("-");
+                question = s.next();
+                a1 = s.next();
+                a2 = s.next();
+                a3 = s.next();
+                a4 = s.next();
+                if (a1.charAt(0) == '*') {
+                    a1 = a1.substring(1);
+                }
+                if (a2.charAt(0) == '*') {
+                    a2 = a2.substring(1);
+                }
+                if (a3.charAt(0) == '*') {
+                    a3 = a3.substring(1);
+                }
+                if (a4.charAt(0) == '*') {
+                    a4 = a4.substring(1);
+                }
+                QandA temp = new QandA(question, a1, a2,a3,a4);
+                list.add(temp);
+                //System.out.println(question + " + " + a1 + " + " + a2 + " + " + a3 + " + " + a4);
+            }
+            s.close();
+        } catch (Exception e) {
+            System.out.println("could not find file");
+        }
+
+        for(int i=0;i<list.size();i++){
+            System.out.println(list.get(i).getQuestion() +" - "+  list.get(i).getAnswer1() +" - "+list.get(i).getAnswer2() +" - "+ list.get(i).getAnswer3()
+            +" - "+list.get(i).getAnswer4());
+        }
+    }
 }
