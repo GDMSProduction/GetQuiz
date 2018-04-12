00ea70c4ee91952b6b67284792e27a27c3e5ab32
Jesus Fragoso <JAFragoso@student.fullsail.edu>
2018-04-12 17:26:26 -0400

    Stuff

--- TriviaGame/app/src/main/java/fullsailclass/triviagame/MainMenu.java
+++ TriviaGame/app/src/main/java/fullsailclass/triviagame/MainMenu.java
@@ -24,6 +24,7 @@ public class MainMenu extends AppCompatActivity {
         configureSoloButton();
         configureSettingsButton();
         setTitle("Main Menu");
+
     }
 
     public void configureSoloButton() {
