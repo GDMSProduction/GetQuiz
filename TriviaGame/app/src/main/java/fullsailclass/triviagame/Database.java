50e1d53b85cb99bf9d038627c2d8a7309974bcc1
Jose Otero Perez <JJOteroPerez@fullsail.edu>
2018-05-10 18:20:34 -0400

    added questions to multiplayer still not sync

---
+++ TriviaGame/app/src/main/java/fullsailclass/triviagame/Database.java
@@ -0,0 +1,51 @@
+package fullsailclass.triviagame;
+public class Database {
+
+    private String A1;
+    private String A2;
+    private String A3;
+    private String A4;
+    private String Q;
+
+
+    public Database() {
+
+    }
+
+    public String getQ() {
+        return Q;
+    }
+
+    public void setQ(String q) {
+        this.Q = Q;
+    }
+
+    public String getA1() {
+        return A1;
+    }
+    public void setA1(String a1) {
+        this.A1 = A1;
+    }
+
+    public String getA2() {
+        return A2;
+    }
+    public void setA2(String a2) {
+        this.A2 = A2;
+    }
+
+    public String getA3() {
+        return A3;
+    }
+    public void setA3(String a3) {
+        this.A3 = A3;
+    }
+
+    public String getA4() {
+        return A4;
+    }
+    public void setA4(String a4) {
+        this.A4 = A4;
+    }
+
+}
